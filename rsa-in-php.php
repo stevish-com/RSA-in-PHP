07cbe426a56dcd8959e4458d64ab99002c16b853
stevish-com <stevish@gmail.com>
2014-04-23 20:08:38 -0500

    Add copyright and GPL notice to rsa-in-php.php
    Oops.

--- rsa-in-php.php
+++ rsa-in-php.php
@@ -8,6 +8,22 @@
 // I only ask that you remove my copyright if you modify and re-release this.
 // To make sure you have the genuine, up-to-date version, visit https://github.com/stevish-com/RSA-in-PHP/
 
+//    Copyright (C) 2009-2014 Stephen Narwold
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License along
+//    with this program; if not, write to the Free Software Foundation, Inc.,
+//    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 // To use:
 //
 // $text = "Peter Piper picked a peck of pickled peppers";
